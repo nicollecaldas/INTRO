d8fcf22587b8e7c0f815307c76390f16ffa96ab2
Nicolle Juliana Caldas Pardo <caldaspardo06@gmail.com>
2025-06-19 23:44:38 -0500

    v.08

--- static/js/programa.js
+++ static/js/programa.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([4.628007164920614, -74.06594569060938], 13);
+var map = L.map('map').setView([4.62064495170278, -74.1078104238216], 19);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
